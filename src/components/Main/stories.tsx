3b9208975d7882804239eba4b7ca5a65fb82e644
Lucas Pazzim <lpazzim@gmail.com>
2021-08-05 11:45:26 -0300

    storybook controls configuration

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,7 +3,11 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'Typescript, ReactJS, NextJS e Styled Components'
+  }
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
